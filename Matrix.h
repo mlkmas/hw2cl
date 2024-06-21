4ea4b60924ebd514dd61c32af81ab3a6ec11792c
malak mas <malakmasarwe669@gmail.com>
2024-06-21 21:29:23 +0300

    Initial commit

--- Matrix.h
+++ Matrix.h
@@ -1,50 +1,62 @@
-
 #ifndef UNTITLED14_MATRIX_H
 #define UNTITLED14_MATRIX_H
 
 #include <iostream>
 #include <cfloat>
 
-template <class T>
+template <int row, int col, typename T=double >
 class Matrix
 {
 private:
     T **data;
     int rows;
-    int columns;
+    int cols;
+
 
 public:
-    Matrix(): data(nullptr), rows(0), columns(0) {}
-    Matrix(int rows, int cols):rows(rows), columns(cols)
+    Matrix(): data(nullptr), rows(0), cols(0) {}
+    Matrix(int rows, int cols): rows(row), cols(col)
     {
         data = new T*[rows];
         for (int i = 0; i < rows; i++)
         {
-            data[i] = new T[columns]();
+            data[i] = new T[col]();
+        }
+    }
+    Matrix(int val) : rows(row), cols(col)
+    {
+        data = new T *[rows];
+        for (int i = 0; i < rows; i++)
+        {
+            data[i] = new T[cols];
+            for (int j = 0; j < cols; j++)
+            {
+                data[i][j] = val;
+            }
         }
     }
     ~Matrix()
     {
         if(data!=nullptr)
         {
-            for(int i=0;i<rows;i++)
+            for(int i=0; i < rows; i++)
             {
                 delete[] data[i];
             }
             delete data;
             rows=0;
-            columns=0;
+            cols=0;
         }
     }
-    Matrix(const Matrix &mat):rows(mat.rows),columns(mat.columns)
+    Matrix(const Matrix &mat): rows(mat.rows), cols(mat.cols)
     {
         if(mat.data!=nullptr)
         {
             data = new T*[rows];
-            for(int i=0;i<rows;i++)
+            for(int i=0; i < rows; i++)
             {
-                data[i] = new T[columns]();
-                for(int j=0;j<columns;j++)
+                data[i] = new T[cols]();
+                for(int j=0; j < cols; j++)
                 {
                     data[i][j]=mat.data[i][j];   //do we have to use get?
                 }
@@ -57,30 +69,53 @@ public:
     }
     int getCols ()const
     {
-        return columns;
+        return cols;
     }
-    T getData (int row,int col)const
-    {
-        if(data!=nullptr && row<rows && col< columns)
-        {
-            return data[row][col];
-        }
-        return 0;//throw exception
-    };
+//    T getData (int rows,int cols)const
+//    {
+//        if(data!=nullptr && rows < rows && cols < cols)
+//        {
+//            return data[rows][cols];
+//        }
+//        return 0;//throw exception
+//    };
 //mat+scalar
-    friend Matrix operator+(const Matrix& mat,const T& scalar)
+    friend Matrix operator+(const T &scalar,Matrix & mat)
     {
         Matrix result(mat);
-
-            for(int i=0;i<result.rows;i++)
+        for(int i=0;i<mat.rows; i++)
+        {
+            for(int j=0;j<mat.cols; j++)
             {
-                for(int j=0;j<result.columns;j++)
-                {
-                    result[i][j]+=scalar;
-
-                }
+                result.data[i][j]=mat.data[i][j]+scalar;
             }
+        }
+        return result;
+    }
+   friend Matrix operator+(Matrix & mat, const T &scalar)
+    {
+        Matrix result(mat);
+        for(int i=0;i<mat.rows; i++)
+        {
+            for(int j=0;j<mat.cols; j++)
+            {
+                result.data[i][j]=mat.data[i][j]+scalar;
+            }
+        }
+        return result;
+    }
+    friend Matrix operator+(const Matrix &mat1, const Matrix &mat2)
+    {
+        //check if they are the same size
 
+        Matrix result(mat1.rows,mat2.cols);
+        for(int i=0;i<mat1.rows; i++)
+        {
+            for(int j=0;j<mat1.cols; j++)
+            {
+                result.data[i][j]=(mat1.data[i][j]+mat2.data[i][j]);
+            }
+        }
         return result;
     }
     //mat-scalar
@@ -95,25 +130,25 @@ public:
     {
         Matrix result(mat);
 
-            for(int i=0;i<mat.rows;i++)
+        for(int i=0;i<mat.row; i++)
+        {
+            for(int j=0;j<mat.col; j++)
             {
-                for(int j=0;j<mat.columns;j++)
-                {
-                    mat[i][j]*=number;
-                }
+                mat[i][j]*=number;
             }
+        }
         return mat;
     }
     friend Matrix operator*(const Matrix& mat1,const Matrix &mat2)
     {
-        if (mat1.columns == mat2.rows)
+        if (mat1.col == mat2.row)
         {
-            Matrix result(mat1.rows,mat2.columns);
-            for(int i=0;i<result.rows;i++)
+            Matrix result(mat1.row, mat2.col);
+            for(int i=0;i<result.row; i++)
             {
-                for(int j=0;j<mat2.columns;j++)
+                for(int j=0;j<mat2.col; j++)
                 {
-                    for(int k=0;k<mat1.columns;k++)
+                    for(int k=0;k<mat1.col; k++)
                     {
                         result.data[i][j]+= (mat1.data[i][k]*mat2.data[k][j]);
                     }
@@ -126,24 +161,24 @@ public:
     }
     Matrix getIdentityMatrix()
     {
-        if(rows != columns)
+        if(row != col)
         {
             //throw exception
         }
 
-        Matrix result(rows,columns);
-        for(int i=0;i<rows;i++)
+        Matrix result(row, col);
+        for(int i=0; i < row; i++)
         {
-            for(int j=0;j<columns;j++)
+            for(int j=0; j < col; j++)
             {
                 result.data[i][j]=(i==j)?1:0;
             }
         }
         return result;
     }
-    friend Matrix operator++(const Matrix &mat1)
+     Matrix operator++(const Matrix &mat1)
     {
-        return (mat1+1);
+        return (mat1 + 1);
     }
     friend Matrix operator--(const Matrix &mat1)
     {
@@ -151,22 +186,23 @@ public:
     }
     T* getDiag(int &number)const
     {
-        int min= (rows < columns)? rows : columns;
-        number=min; //?
-        T *diag= new T*[min];
+        int min= (row < col) ? row : col;
+
+        T *diag= new T[min];
         for(int i=0;i<min;i++)
         {
             diag[i]=data[i][i];
         }
+        number=min;
         return diag;
     }
     friend bool operator==(const Matrix& mat1, const Matrix& mat2)
     {
-        if(mat1.rows!=mat2.rows || mat1.columns!= mat2.columns)
+        if(mat1.row != mat2.row || mat1.col != mat2.col)
             return false;
-        for(int i=0;i<mat1.rows;i++)
+        for(int i=0;i<mat1.row; i++)
         {
-            for(int j=0;j<mat1.columns;j++)
+            for(int j=0;j<mat1.col; j++)
             {
                 T diff=mat1.data[i][j]-mat2.data[i][j];
                 diff= (diff>=0)?diff:-diff;
@@ -181,10 +217,10 @@ public:
     {
         return !(mat1==mat2);
     }
-     operator T()const
+    operator T()const
     {
         T trace=0;
-        int minLen= (rows< columns)? rows:columns;
+        int minLen= (row < col) ? row : col;
         for(int i=0;i<minLen;i++)
         {
             trace+=data[i][i];
@@ -193,9 +229,9 @@ public:
     }
     friend std::ostream& operator<<(std::ostream& os, const Matrix& mat)
     {
-        for(int i=0;i<mat.rows;i++)
+        for(int i=0;i<mat.rows; i++)
         {
-            for(int j=0;j<mat.columns;j++)
+            for(int j=0;j<mat.cols; j++)
             {
                 os<<mat.data[i][j]<<' ';
             }
@@ -204,7 +240,7 @@ public:
         }
         return os;
     }
-     T& operator()( const int i,const int j)
+    T& operator()( const int i,const int j)
     {
         return data[i][j];
     }
